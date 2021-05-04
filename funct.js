4fed2f0d6599c5f7098547fd08c7dd89b456e5c0
Emanuel Santos <79867442+mizaelucas@users.noreply.github.com>
2021-05-03 22:59:06 -0300

    Create funct.js

---
+++ funct.js
@@ -0,0 +1,7 @@
+let operacao = prompt("Qual o nome do agente de Valorant abaixo?")
+
+if (operacao == "Sage" || operacao == "sage") {
+  document.write("<h2>" + "Certa resposta!" + "</h2>")
+} else {
+  document.write("<h2>" + "Você errou! :(" + "</h2>")
+}
